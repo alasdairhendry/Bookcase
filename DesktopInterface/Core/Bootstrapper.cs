61c29b5ec8404b2f86166f86b7d7810787ce9b4c
alasdairhendry <alasdairhendry@gmail.com>
2023-09-01 15:46:32 +0100

    Configured Caliburn.Micro and added Dependency Injection
    * Set up initial changes that are required for Caliburn.Micro to turn
    * ShellView has been added and configured
    * DI has been configured for use with app and C.M in general
    * Appsettings configured to .NET Core standards

---
+++ DesktopInterface/Core/Bootstrapper.cs
@@ -0,0 +1,61 @@
+﻿using Caliburn.Micro;
+using DesktopInterface.ViewModels;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace DesktopInterface.Core
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer container = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            container.Instance(container);
+
+            // Configure Per Request types
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true).Build();
+            container.Instance(config);
+
+            // Configure Singleton types
+            container.Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            // Connect our ViewModels to our Views using reflection
+            // These are PER REQUEST!
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => container.RegisterPerRequest(viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
+    }
+}
---
+++ DesktopInterface/ViewModels/ShellViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using Microsoft.Extensions.Configuration;
+
+namespace DesktopInterface.ViewModels
+{
+    internal class ShellViewModel : Conductor<object>
+    {
+        private string _applicationVersion;
+
+        public string ApplicationVersion
+        {
+            get { return $"v{_applicationVersion}"; }
+            set
+            {
+                _applicationVersion = value;
+                NotifyOfPropertyChange(() => ApplicationVersion);
+            }
+        }
+
+        public ShellViewModel(IConfiguration config)
+        {
+            _applicationVersion = config["app-version"] ?? string.Empty;
+        }
+    }
+}
