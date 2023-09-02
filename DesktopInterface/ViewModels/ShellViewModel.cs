ffae8c937a284aab4826fe36ed2819a2f8d8e1dd
alasdairhendry <alasdairhendry@gmail.com>
2023-09-02 02:07:46 +0100

    Update how versioning is displayed

--- DesktopInterface/ViewModels/ShellViewModel.cs
+++ DesktopInterface/ViewModels/ShellViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using Caliburn.Micro;
 using Microsoft.Extensions.Configuration;
+using System.Text.RegularExpressions;
 
 namespace DesktopInterface.ViewModels
 {
     internal class ShellViewModel : Conductor<object>
     {
-        private string _applicationVersion;
+        private string _applicationVersion = string.Empty;
+        private string _applicationVersionStage = string.Empty;
 
         public string ApplicationVersion
         {
@@ -13,13 +15,33 @@ namespace DesktopInterface.ViewModels
             set
             {
                 _applicationVersion = value;
-                NotifyOfPropertyChange(() => ApplicationVersion);                
+                NotifyOfPropertyChange(() => ApplicationVersion);
+            }
+        }
+
+        public string ApplicationVersionStage
+        {
+            get { return _applicationVersionStage; }
+            set
+            {
+                _applicationVersionStage = value;
+                NotifyOfPropertyChange(() => ApplicationVersionStage);
             }
         }
 
         public ShellViewModel(IConfiguration config)
         {
             _applicationVersion = config["app-version"] ?? string.Empty;
+
+            if (Regex.Match(_applicationVersion, "^\\d+.\\d+.\\d+$").Success)
+            {
+                ApplicationVersionStage = "stable";
+            }
+            else
+            {
+                ApplicationVersionStage = "[PRE-RELEASE]";
+            }
+
             ActivateItemAsync(IoC.Get<BookcaseViewModel>());
         }
     }
